de4aac82400e58185c7c043fd9632535885bfa00
Kevin Christ <christkevin50@gmail.com>
2024-12-25 08:43:01 +0100

    created a navbar component
    added a navbar with 2 divs. links are still empty since the
    section does not exist yet.

---
+++ src/routes/navbar.svelte
@@ -0,0 +1,12 @@
+
+
+<nav>
+    <div class="logo">
+        kchrist
+    </div>
+    <div class="links">
+        <a href="">Projects</a>
+        <a href="">Skills and Tools</a>
+        <a href="">Contact</a>
+    </div>
+</nav>
