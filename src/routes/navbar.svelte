7e48d78e8ecc2e748b6746d10287c60ffb5b2b57
Kevin Christ <christkevin50@gmail.com>
2024-12-25 09:13:14 +0100

    adjusted the styling of the navbar

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,8 +1,6 @@
-
-
 <nav>
     <div class="logo">
-        kchrist
+        kchrist.
     </div>
     <div class="links">
         <a href="">Projects</a>
@@ -10,3 +8,29 @@
         <a href="">Contact</a>
     </div>
 </nav>
+
+<style>
+    nav{
+        font-family: "Chillax", serif;
+        font-size: 2.5rem;
+        font-weight: 700;
+
+        background-color: #313dc4;
+
+        display: flex;
+        justify-content: space-between;
+    }
+    .logo{
+        color: white;    
+    }
+    
+    a{
+       padding: 0 1rem;
+       text-decoration: none;
+       color: white;
+    }
+    a:hover{
+        color: #f8d112;
+        text-decoration: overline;
+    }
+</style>
